5a07a0823c92520475be0b78beafe946dc7f4a73
CoderRoy05 <royalalawode@gmail.com>
2023-09-29 20:27:07 +0100

    bounce style on header image

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
           <p>795,600 people requested access a visit in last 24 hours</p>
         </div>
       </div>
-      <div className="gpt3__header-image">
+      <div className="gpt3__header-image" id="bouncing-div">
         <img src={ai} alt="ai" />
       </div>
       
